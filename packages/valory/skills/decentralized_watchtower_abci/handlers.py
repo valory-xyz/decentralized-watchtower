e8c5cda96b65141368ccd6f00fe776672b3a7e28
Ardian <ardianabazi09@gmail.com>
2023-08-29 20:44:45 +0200

    feat: add healthcheck

--- packages/valory/skills/decentralized_watchtower_abci/handlers.py
+++ packages/valory/skills/decentralized_watchtower_abci/handlers.py
@@ -18,6 +18,15 @@
 # ------------------------------------------------------------------------------
 
 """This module contains the handlers for the skill of CowOrdersAbciApp."""
+import json
+import re
+from ctypes import cast
+from datetime import datetime
+from enum import Enum
+from typing import Optional, Tuple, Callable, Dict
+from urllib.parse import urlparse
+
+from aea.protocols.base import Message
 
 from packages.valory.skills.abstract_round_abci.handlers import (
     ABCIRoundHandler as BaseABCIRoundHandler,
@@ -40,12 +49,212 @@ from packages.valory.skills.abstract_round_abci.handlers import (
 from packages.valory.skills.abstract_round_abci.handlers import (
     TendermintHandler as BaseTendermintHandler,
 )
-
+from packages.fetchai.connections.http_server.connection import (
+    PUBLIC_ID as HTTP_SERVER_PUBLIC_ID,
+)
+from packages.valory.skills.decentralized_watchtower_abci.models import SharedState
+from packages.valory.skills.cow_orders_abci.rounds import SynchronizedData
+from packages.valory.skills.decentralized_watchtower_abci.dialogues import HttpDialogues, HttpDialogue
+from packages.valory.protocols.http.message import HttpMessage
 
 ABCIHandler = BaseABCIRoundHandler
-HttpHandler = BaseHttpHandler
 SigningHandler = BaseSigningHandler
 LedgerApiHandler = BaseLedgerApiHandler
 ContractApiHandler = BaseContractApiHandler
 TendermintHandler = BaseTendermintHandler
 IpfsHandler = BaseIpfsHandler
+
+AVERAGE_PERIOD_SECONDS = 10
+WEBSOCKET_CLIENT_CONNECTION_NAME = "websocket_client"
+
+OK_CODE = 200
+NOT_FOUND_CODE = 404
+BAD_REQUEST_CODE = 400
+
+class HttpMethod(Enum):
+    """Http methods"""
+
+    GET = "get"
+    HEAD = "head"
+    POST = "post"
+
+
+class HttpHandler(BaseHttpHandler):
+    """This implements the echo handler."""
+
+    SUPPORTED_PROTOCOL = HttpMessage.protocol_id
+
+    def setup(self) -> None:
+        """Implement the setup."""
+        health_url_regex = r'^/healthcheck$'
+        self.routes = {
+            (HttpMethod.GET.value, HttpMethod.HEAD.value): [
+                (health_url_regex, self._handle_get_health),
+            ],
+        }
+        self.json_content_header = "Content-Type: application/json\n"
+
+        for (
+            connection
+        ) in self.context.outbox._multiplexer.connections:  # pylint: disable=W0212
+            if connection.component_id.name == WEBSOCKET_CLIENT_CONNECTION_NAME:
+                self._ws_client_connection = connection
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return SynchronizedData(
+            db=self.context.state.round_sequence.latest_synchronized_data.db
+        )
+
+    def _get_handler(self, url: str, method: str) -> Tuple[Optional[Callable], Dict]:
+        """Check if an url is meant to be handled in this handler
+
+        We expect url to match the pattern {hostname}/.*,
+        where hostname is allowed to be localhost, 127.0.0.1 or the token_uri_base's hostname.
+        Examples:
+            localhost:8000/0
+            127.0.0.1:8000/100
+
+        :param url: the url to check
+        :returns: the handling method if the message is intended to be handled by this handler, None otherwise, and the regex captures
+        """
+        # Check if there is a route for this request
+        for methods, routes in self.routes.items():
+            if method not in methods:
+                continue
+
+            return self._handle_get_health, {}
+
+        # No route found
+        self.context.logger.info(
+            f"The message [{method}] {url} is intended for the DecentralizedWatchtower HttpHandler but did not match any valid pattern"
+        )
+        return self._handle_bad_request, {}
+
+    def handle(self, message: Message) -> None:
+        """
+        Implement the reaction to an envelope.
+
+        :param message: the message
+        """
+
+        # Check if this is a request sent from the http_server skill
+        if (
+            message.performative != HttpMessage.Performative.REQUEST
+            or message.sender != str(HTTP_SERVER_PUBLIC_ID.without_hash())
+        ):
+            super().handle(message)
+            return
+
+        # Retrieve dialogues
+        http_dialogues = self.context.http_dialogues
+        http_dialogue = http_dialogues.update(message)
+
+        # Invalid message
+        if http_dialogue is None:
+            self.context.logger.info(
+                "Received invalid http message={}, unidentified dialogue.".format(
+                    message
+                )
+            )
+            return
+
+        # Handle message
+        self.context.logger.info(
+            "Received http request with method={}, url={} and body={!r}".format(
+                message.method,
+                message.url,
+                message.body,
+            )
+        )
+        self._handle_get_health(message, http_dialogue)
+
+    def _handle_bad_request(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue
+    ) -> None:
+        """
+        Handle a Http bad request.
+
+        :param http_msg: the http message
+        :param http_dialogue: the http dialogue
+        """
+        http_response = http_dialogue.reply(
+            performative=HttpMessage.Performative.RESPONSE,
+            target_message=http_msg,
+            version=http_msg.version,
+            status_code=BAD_REQUEST_CODE,
+            status_text="Bad request",
+            headers=http_msg.headers,
+            body=b"",
+        )
+
+        # Send response
+        self.context.logger.info("Responding with: {}".format(http_response))
+        self.context.outbox.put_message(message=http_response)
+
+    def _handle_get_health(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue
+    ) -> None:
+        """
+        Handle a Http request of verb GET.
+
+        :param http_msg: the http message
+        :param http_dialogue: the http dialogue
+        """
+        try:
+            is_connected = self._ws_client_connection.is_connected
+            is_tm_unhealthy = self.context.state.round_sequence.block_stall_deadline_expired
+            is_healthy = all(
+                [
+                    is_connected,
+                    not is_tm_unhealthy,
+                ]
+            )
+            data = {
+                "healthy": is_healthy,
+                "period": self.synchronized_data.period_count,
+                "tm_ok": not is_tm_unhealthy,
+                "web3_ok": is_connected,
+            }
+        except (AttributeError, KeyError, ValueError):
+            data = {
+                "healthy": False,
+                "reason": "Not ready."
+            }
+        self._send_ok_response(http_msg, http_dialogue, data)
+
+    def _send_ok_response(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue, data: Dict
+    ) -> None:
+        """Send an OK response with the provided data"""
+        http_response = http_dialogue.reply(
+            performative=HttpMessage.Performative.RESPONSE,
+            target_message=http_msg,
+            version=http_msg.version,
+            status_code=OK_CODE,
+            status_text="Success",
+            headers=f"{self.json_content_header}{http_msg.headers}",
+            body=json.dumps(data).encode("utf-8"),
+        )
+
+        # Send response
+        self.context.logger.info("Responding with: {}".format(http_response))
+        self.context.outbox.put_message(message=http_response)
+
+    def _send_not_found_response(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue
+    ) -> None:
+        """Send an not found response"""
+        http_response = http_dialogue.reply(
+            performative=HttpMessage.Performative.RESPONSE,
+            target_message=http_msg,
+            version=http_msg.version,
+            status_code=NOT_FOUND_CODE,
+            status_text="Not found",
+            headers=http_msg.headers,
+            body=b"",
+        )
+        # Send response
+        self.context.logger.info("Responding with: {}".format(http_response))
+        self.context.outbox.put_message(message=http_response)
